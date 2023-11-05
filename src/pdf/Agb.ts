d106aa7ba9d21f595abb4d0c80684450118244cb
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-11-05 17:57:53 +0100

    add text

--- src/pdf/Agb.ts
+++ src/pdf/Agb.ts
@@ -1,128 +1,151 @@
-const agb = {
-  agb1: [
-    '§1. Leistungen des Möbelspediteurs.',
-
-    'Der Möbelspediteur führt unter Wahrung des Interesses des Absenders seine Verpflichtungen mit der ' +
-      'verkehrsüblichen Sorgfalt eines ordentlich Möbelspediteurs gegen Bezahlung des vereinbarten Entgelts' +
-      ' aus. Zusätzlich zu vergüten sind besondere, bei der Vertragsabschluß nicht vorhersehbare ' +
-      'Leistungen und Aufwendungen. Gleiches gilt, wenn der Leistungsumfang durch den Absender nach' +
-      ' Vertragsabschluß erweitert wird. Die Pflichten des Frachtführers umfassen auch das Ab- und Aufbauen' +
-      ' der Möbel sowie die Verpackung des Umzugsgutes. Das Gewicht der Umzugskartons darf nicht 20 kg überschreiten.',
-    '§2. Beauftragung eines weiteren Frachtführers.',
-
-    'Der Möbelspediteur kann einen weiteren Frachtführer zur Durchführung des Umzugs heranziehen.',
-
-    '§3. Trinkgeld.',
-    'Trinkgelder sind mit der Rechnung des Möbelspediteurs nicht verrechenbar.',
-    '§4. Erstattung der Umzugs- und Lagerkosten.',
-
-    'Soweit der Absender gegenüber einer Dienststelle oder einem Arbeitgeber einen' +
+const agb = [
+  {
+    title: 'Leistungen des Möbelspediteurs.',
+    text:
+      'Der Möbelspediteur führt unter Wahrung des Interesses des Absenders seine Verpflichtungen mit der ' +
+      'verkehrsüblichen Sorgfalt eines ordentlich Möbelspediteurs gegen Bezahlung des vereinbarten Entgelts ' +
+      'aus. Zusätzlich zu vergüten sind besondere, bei der Vertragsabschluß nicht vorhersehbare ' +
+      'Leistungen und Aufwendungen. Gleiches gilt, wenn der Leistungsumfang durch den Absender nach ' +
+      'Vertragsabschluß erweitert wird.',
+  },
+  {
+    title: 'Eigenverpackung und Möbelvorbereitung beim Umzug',
+    text:
+      'Sobald die Auftraggeber*innen die Umzugskartons selbst packen, ' +
+      'dürfen die einzelnen Kartons ein Gewicht von maximal 20 kg haben. ' +
+      'Wenn die Möbel vom Auftraggeber vorbereitet werden, müssen sie vollständig von fremden Gegenständen befreit sein.',
+  },
+  {
+    title: 'Beauftragung eines weiteren Frachtführers.',
+    text: 'Der Möbelspediteur kann einen weiteren Frachtführer zur Durchführung des Umzugs heranziehen.',
+  },
+  {
+    title: 'Trinkgeld',
+    text: 'Trinkgelder sind mit der Rechnung des Möbelspediteurs nicht verrechenbar.',
+  },
+  {
+    title: 'Erstattung der Umzugs- und Lagerkosten.',
+    text:
+      'Soweit der Absender gegenüber einer Dienststelle oder einem Arbeitgeber einen' +
       'Anspruch auf Umzugskostenvergütung hat, weist er diese Stelle noch bei Umzugsauftrag' +
       'schriftlich an. Generell sind die vereinbarten und fälligen Umzugskostenvergütungen abzüglich ' +
-      'geleisteten Anzahlungen oder Teilzahlungen sofort nach dem Umzugsende auf entsprechende Anforderung' +
-      ' direkt an den Möbelspediteur auszuzahlen. Entgelte für einen Lagervertrag (Einlagerung) werden grundsätzlich' +
+      'geleisteten Anzahlungen oder Teilzahlungen sofort nach dem Umzugsende auf entsprechende Anforderung ' +
+      'direkt an den Möbelspediteur auszuzahlen. Entgelte für einen Lagervertrag (Einlagerung) werden grundsätzlich ' +
       'in der letzten Monatswoche im Voraus für den nächsten Kalendermonat per Lastschrift abgezogen.',
+  },
+  {
+    title: 'Transportsicherungen.',
+    text:
+      'Der Absender ist verpflichtet, bewegliche oder elektronische Teile an hochempfindlichen Geräten wie z.B. ' +
+      'Waschmaschinen, Plattenspielern, Fernseh-, Radio-, und HiFi - Geräten, EDV-Anlagen fachgerecht für den ' +
+      'Transport zu sichern. Zur Überprüfung der fachgerechten Transportsicherung ist der Möbelspediteur nicht verpflichtet.',
+  },
 
-    '§5. Transportsicherungen.',
-
-    'Der Absender ist verpflichtet, bewegliche oder elektronische Teile an hochempfindlichen Geräten wie z.B.' +
-      ' Waschmaschinen, Plattenspielern, Femseh-, Radio-, und HiFi - Geräten, EDV-Anlagen fachgerecht für den' +
-      ' Transport zu sichern. Zur Überprüfung der fachgerechten Transportsicherung ist der Möbelspediteur nicht verpflichtet.',
-
-    '§6. Handwerkervermittlung.',
-    'Bei Leistungen zusätzlich vermittelter Handwerker haftet der Möbelspediteur nicht.',
-
-    '§7. Elektro- und Installationsarbeiten.',
-
-    'Die Leute des Möbelspediteurs sind, sofern nichts anderes vereinbart ist, nicht zur Vornahmen von ' +
+  {
+    title: 'Handwerkervermittlung.',
+    text: 'Bei Leistungen zusätzlich vermittelter Handwerker haftet der Möbelspediteur nicht.',
+  },
+  {
+    title: 'Elektro- und Installationsarbeiten.',
+    text:
+      'Die Leute des Möbelspediteurs sind, sofern nichts anderes vereinbart ist, nicht zur Vornahmen von ' +
       'Elektro-, Gas-, Wasser-, Dübeln- und sonstigen Installationsarbeiten berechtigt.',
+  },
 
-    '§8. Aufrechnung.',
-
-    'Gegen Ansprüche des Möbelspediteurs ist eine Aufrechnung nur mit fälligen Gegenansprüchen ' +
+  {
+    title: 'Aufrechnung.',
+    text:
+      'Gegen Ansprüche des Möbelspediteurs ist eine Aufrechnung nur mit fälligen Gegenansprüchen ' +
       'zulässig, die unbestritten oder rechtskräftig festgestellt ist.',
-
-    '§9. Missverständnisse.',
-
-    'Die Gefahr des Missverständnisses anderer als schriftlicher Auftragsbestätigungen, Weisungen' +
+  },
+  {
+    title: 'Missverständnisse.',
+    text:
+      'Die Gefahr des Missverständnisses anderer als schriftlicher Auftragsbestätigungen, Weisungen' +
       ' und Mitteilungen des Absenders und solche an andere zu ihrer Annahme nicht bevollmächtigte ' +
       'Leute des Möbelspediteurs hat der letztere nicht zu verantworten.',
-
-    '§10. Nachprüfung durch den Absender.',
-
-    'Bei der Abholung, Ver- oder Entladung des Umzugsgutes ist der Absender verpflichtet, nachzuprüfen,' +
-      ' daß kein Gegenstand oder keine Einrichtung irrtümlich mitgenommen oder stehengelassen wurde. ' +
+  },
+  {
+    title: 'Nachprüfung durch den Absender.',
+    text:
+      'Bei der Abholung, Ver- oder Entladung des Umzugsgutes ist der Absender verpflichtet, nachzuprüfen, ' +
+      'dass kein Gegenstand oder keine Einrichtung irrtümlich mitgenommen oder stehengelassen wurde. ' +
       'Äußerlich erkennbare Verluste oder Beschädigungen sind detailliert auf dem Arbeitsschein (Umzugsauftrag) ' +
       'schriftlich festzuhalten. Pauschale Schadenhinweise sind nicht ausreichend',
-
-    '§11. Fälligkeit des vereinbarten Entgelts.',
-
-    'Der Rechnungsbetrag ist bei Inlandstransporten vor Beendigung der Entladung, bei Auslandstransporten ' +
+  },
+  {
+    title: 'Fälligkeit des vereinbarten Entgelts.',
+    text:
+      'Der Rechnungsbetrag ist bei Inlandstransporten vor Beendigung der Entladung, bei Auslandstransporten ' +
       'vor Beginn der Verladung fällig und in Bar oder Form gleichwertiger Zahlungsmittel zu bezahlen. ' +
-      'Barzahlungen in ausländischer Währung sind nach dem abgerechneten Wechselkurs zu Entrichten. Kommt' +
-      ' der Absender/Einlagerer seiner Zahlungsverpflichtung nicht nach, ist der Möbelspediteur berechtigt,' +
-      ' das Umzugsgut anzuhalten oder nach Beginn der Beförderung auf Kosten des Absenders einzulagern. §419 HGB findet entsprechende Anwendung.',
-
-    '§12. Pfandrecht des Möbelspediteurs.',
-
-    'Macht der Möbelspediteur von seinem Recht zum Pfandverkauf der in seinen Besitz gelangten Gegenstände ' +
-      'Gebrauch, so genügt für die Pfandversteigerungsandrohung und die Mitteilung des Versteigerungstermines die' +
-      ' Absendung einer Benachrichtigung an die letzte dem Möbelspediteur bekannte Anschrift des Einlagerers.' +
-      ' Die Pfandversteigerung darf nicht vor dem Ablauf eines Monats nach ihrer Androhung erfolgen.',
-
-    '§13. Haftungshöchstbetrag.',
-
-    'Die Haftung des Frachtführers wegen Verlust oder Beschädigung der gesamten Sendung ist auf einen Betrag von ' +
-      ' 8,33 Rechnungseinheiten für jedes Kilogramm des Rohgewicht der Sendung begrenzt. Bei der Erfüllung des ' +
+      'Kommt der Absender/Einlagerer seiner Zahlungsverpflichtung nicht nach, ist der Möbelspediteur berechtigt, ' +
+      'das Umzugsgut anzuhalten oder nach Beginn der Beförderung auf Kosten des Absenders einzulagern. §419 HGB findet entsprechende Anwendung.',
+  },
+  {
+    title: 'Pfandrecht des Möbelspediteurs.',
+    text:
+      'Macht der Möbelspediteur von seinem Recht zum Pfandverkauf der in seinen Besitz gelangten Gegenstände ' +
+      'Gebrauch, so genügt für die Pfandversteigerungsandrohung und die Mitteilung des Versteigerungstermines die ' +
+      'Absendung einer Benachrichtigung an die letzte dem Möbelspediteur bekannte Anschrift des Einlagerers. ' +
+      'Die Pfandversteigerung darf nicht vor dem Ablauf eines Monats nach ihrer Androhung erfolgen.',
+  },
+  {
+    title: 'Haftungshöchstbetrag.',
+    text:
+      'Die Haftung des Frachtführers wegen Verlust oder Beschädigung der gesamten Sendung ist auf einen Betrag von ' +
+      '8,33 Rechnungseinheiten für jedes Kilogramm des Rohgewicht der Sendung begrenzt. Bei der Erfüllung des ' +
       'Umzugsvertrages ist Haftungshöchstbetrag auf 620,00 EUR/Kubikmeter begrenzt. Maßgeblich für die Berechnung ' +
       'dieser Haftungsbegrenzung ist der zur Erfüllung des Umzugsvertrages benötigte Rauminhalt. Die Haftungsbegrenzung ' +
       'kann durch eine Wertdeklaration bei Vertragsabschluß auf den deklarierten Betrag angehoben werden. ' +
-      'Maßgeblich ist der Betrag, der bei Anschaffung von Gegenständen gleicher Art und Güte aufgewendet werden muß,' +
-      ' wobei Abzüge "neu für alt" zu berücksichtigen sind. Weitergehende Schadenersatzleistungen können durch eine ' +
+      'Maßgeblich ist der Betrag, der bei Anschaffung von Gegenständen gleicher Art und Güte aufgewendet werden muss, ' +
+      'wobei Abzüge "neu für alt" zu berücksichtigen sind. Weitergehende Schadenersatzleistungen können durch eine ' +
       'Umzugstransportversicherung auf Neuwertbasis durch Vermittlung des Möbelspediteurs erreicht werden.',
+  },
 
-    '§14. Schadensanzeige.',
-
-    'Ansprüche wegen Verlust oder die Beschädigung des Gutes erlöschen: a) wenn der Verlust oder die Beschädigung des Gutes' +
-      ' äußerlich erkennbar war und dem Möbelspediteur nicht spätestens am Tag nach der Ablieferung schriftlich angezeigt ' +
-      'worden ist (es gilt das Datum des Poststempels) b) wenn der Verlust oder die Beschädigung äußerlich nicht erkennbar' +
-      ' war und dem Frachtführer nicht innerhalb von vierzehn Tagen nach Ablieferung schriftlich angezeigt worden ist. Das' +
-      ' heißt, das Reklamationsschreiben muß innerhalb von vierzehn Tagen dem Möbelspediteur zugehen. Bei dieser nachträglichen' +
-      ' Reklamation ist auch der Nachweis zu führen, dass der Schaden während der dem Möbelspediteur obliegenden Behandlung des ' +
+  {
+    title: 'Schadensanzeige.',
+    text:
+      'Ansprüche wegen Verlust oder die Beschädigung des Gutes erlöschen: a) wenn der Verlust oder die Beschädigung des Gutes ' +
+      'äußerlich erkennbar war und dem Möbelspediteur nicht spätestens am Tag nach der Ablieferung schriftlich angezeigt ' +
+      'worden ist (es gilt das Datum des Poststempels) b) wenn der Verlust oder die Beschädigung äußerlich nicht erkennbar ' +
+      'war und dem Frachtführer nicht innerhalb von vierzehn Tagen nach Ablieferung schriftlich angezeigt worden ist. Das ' +
+      'heißt, das Reklamationsschreiben muß innerhalb von vierzehn Tagen dem Möbelspediteur zugehen. Bei dieser nachträglichen ' +
+      'Reklamation ist auch der Nachweis zu führen, dass der Schaden während der dem Möbelspediteur obliegenden Behandlung des ' +
       'Gutes entstanden ist.',
-
-    '§15. Abwesenheit des Absenders.',
-
-    'Bei der Abwesenheit des Absenders muß dieser eine Vertretung festlegen, um §10 und §14 einzuhalten,' +
+  },
+  {
+    title: 'Abwesenheit des Absenders.',
+    text:
+      'Bei der Abwesenheit des Absenders muß dieser eine Vertretung festlegen, um §10 und §14 einzuhalten,' +
       ' sonst erlöschen Ansprüche wegen Verlust oder Beschädigung des Gutes.',
+  },
 
-    '§16. Erweiterungen des Leistungsumfangs.',
-
-    'Der Umzugspreis berechnet sich anhand der, von dem/der Auftraggeber(in) zur Verfügung gestellten' +
+  {
+    title: 'Erweiterungen des Leistungsumfangs.',
+    text:
+      'Der Umzugspreis berechnet sich anhand der, von dem/der Auftraggeber(in) zur Verfügung gestellten' +
       ' Daten aus dem Online Formular' +
       ' Werden dem Unternehemn Details des Umzuges fehlerhaft oder unvollständig übertragen, behält sich das Unternehmen' +
       ' das Recht vor, den Umzugspreis entsprechend der Liste "zusätzliche Leistungen" anzupassen.',
-  ],
-  agb2: [
-    '§17. Besondere Haftungsausschlußgründe.',
-
-    'Der Frachtführer ist von seiner Haftung befreit, soweit der Verlust oder die Beschädigung auf eine der folgenden Gefahren zurückzuführen ist:' +
-      ' a) Beförderung von Edelmetallen, Juwelen, Edelsteinen, Geld, Briefmarken, Münzen, Wertpapieren oder Urkunden' +
-      ' b) ungenügende Verpackung oder Kennzeichnung durch den Absender c) Behandeln, Verladen oder Entladen des' +
-      ' Gutes durch den Absender oder einen dritten Möbelspediteur (zum Beispiel Zwischenlagerung) d)' +
-      ' Beförderung von nicht vom Frachtführer verpacktem Gut in Behältern, (bzw. in Umzugskartons) e)' +
-      ' Verladen oder Entladen von Gut, dessen Größe oder Gewicht den Raumverhältnissen an der Ladestelle oder' +
-      ' Entladestelle nicht entspricht(u.a. Klavierbeförderung in engen Räumen) f) Beförderung lebender' +
-      ' Tiere oder von Pflanzen g) natürliche oder mangelhafte Beschaffenheit des Gutes, der zufolge es ' +
+    prices: true,
+  },
+  {
+    title: 'Besondere Haftungsausschlußgründe.',
+    text:
+      'Der Frachtführer ist von seiner Haftung befreit, soweit der Verlust oder die Beschädigung auf eine der folgenden Gefahren zurückzuführen ist: ' +
+      'a) Beförderung von Edelmetallen, Juwelen, Edelsteinen, Geld, Briefmarken, Münzen, Wertpapieren oder Urkunden ' +
+      'b) ungenügende Verpackung oder Kennzeichnung durch den Absender c) Behandeln, Verladen oder Entladen des ' +
+      'Gutes durch den Absender oder einen dritten Möbelspediteur (zum Beispiel Zwischenlagerung) d)' +
+      'Beförderung von nicht vom Frachtführer verpacktem Gut in Behältern, (bzw. in Umzugskartons) e) ' +
+      'Verladen oder Entladen von Gut, dessen Größe oder Gewicht den Raumverhältnissen an der Ladestelle oder ' +
+      'Entladestelle nicht entspricht(u.a. Klavierbeförderung in engen Räumen) f) Beförderung lebender ' +
+      'Tiere oder von Pflanzen g) natürliche oder mangelhafte Beschaffenheit des Gutes, der zufolge es ' +
       'besonders leicht Schäden, insbesondere durch Bruch, Funktionsstörungen, Rost, inneren Verderb oder Auslaufen, erleidet.',
-
-    '§18. Gerichtsstand.',
-
-    'Für Rechtsstreitigkeiten ist das Gericht, in dessen Bezirk sich die vom Absender beauftragte Firma befindet, ausschließlich zuständig.',
-
-    '§19. Vereinbarung deutschen Rechts.',
-    'Es gilt deutsches Recht.',
-  ],
-};
+  },
+  {
+    title: 'Gerichtsstand und Recht',
+    text: 'Für Rechtsstreitigkeiten ist das Gericht, in dessen Bezirk sich die vom Absender beauftragte Firma befindet, ausschließlich zuständig. Es gilt deutsches Recht',
+  },
+];
 
 export default agb;
