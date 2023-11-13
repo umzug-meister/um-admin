abef3a1e79e7283732b6549d98a205181d254f80
Nikolai K <12097812+mee3003@users.noreply.github.com>
2023-11-13 18:26:11 +0100

    split email template (#153)
    * Revert "refactor email template, add 'only services' block"
    
    This reverts commit 2f204560adc9c6697b73899a5dca1f6ae2332554.
    
    * Revert "Revert "refactor email template, add 'only services' block""
    
    This reverts commit 765d63ad5b542d1d35d533776dd4cab009e33ae9.
    
    * refactor tables
    
    * Revert "Merge branch 'main' into feat/template"
    
    This reverts commit 1cace6170914a96421d2447c85de5525b53eb9fd, reversing
    changes made to b5f5c3f4130687322842c6f2fc2cf4e85ab4b8bf.

---
+++ src/components/email-text-blocks/EmailServicesTable.tsx
@@ -0,0 +1,46 @@
+import { euroValue } from '../../utils/utils';
+
+import { Order } from 'um-types';
+
+interface Props {
+  order: Order;
+}
+
+export function EmailServicesTable({ order }: Readonly<Props>) {
+  const { leistungen = [], timeBased, sum } = order;
+
+  return (
+    <table style={{ maxWidth: '600px', borderBottom: '1px solid black' }}>
+      <tbody>
+        {leistungen
+          .filter((l) => l.hidden !== true)
+          .map((lst) => (
+            <ServicesTableRow key={lst.desc} desc={lst.desc} price={lst.sum} red={lst.red} />
+          ))}
+        <ServicesTableRow
+          desc={`Gesamtbetrag ${timeBased?.hours ? `(${timeBased.hours} Stunden)` : ''}`}
+          price={sum}
+          bold
+        />
+      </tbody>
+    </table>
+  );
+}
+
+interface RowProps {
+  desc: string;
+  price: string | number;
+  red?: boolean;
+  bold?: boolean;
+}
+
+function ServicesTableRow({ desc, price, red, bold }: Readonly<RowProps>) {
+  const color = red ? 'red' : 'black';
+
+  return (
+    <tr style={{ color }}>
+      {bold ? <th align="left">{desc}</th> : <td align="left">{desc}</td>}
+      <th align="right">{euroValue(price)} inkl. MwSt.</th>
+    </tr>
+  );
+}
