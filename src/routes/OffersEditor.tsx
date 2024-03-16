b4cbd19f345188f319aff33b742eb29d4952df9b
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2024-03-16 19:59:02 +0100

    feat: add more offer

--- src/routes/OffersEditor.tsx
+++ src/routes/OffersEditor.tsx
@@ -8,16 +8,26 @@ import { AppGridContainer } from '../components/shared/AppGridContainer';
 
 export default function OffersEditor() {
   return (
-    <AppGridContainer>
-      <OfferCardWrapper t35={1} workers={2} />
-      <OfferCardWrapper t35={1} workers={3} />
-      <OfferCardWrapper t35={1} workers={4} />
-      <OfferCardWrapper t35={2} workers={4} />
-      <OfferCardWrapper t35={2} workers={5} />
-      <OfferCardWrapper t35={2} workers={6} />
-      <OfferCardWrapper t35={3} workers={5} />
-      <OfferCardWrapper t35={3} workers={6} />
-    </AppGridContainer>
+    <>
+      <AppGridContainer>
+        <OfferCardWrapper t35={1} workers={2} />
+        <OfferCardWrapper t35={1} workers={3} />
+        <OfferCardWrapper t35={1} workers={4} />
+      </AppGridContainer>
+      <AppGridContainer>
+        <OfferCardWrapper t35={2} workers={4} />
+        <OfferCardWrapper t35={2} workers={5} />
+        <OfferCardWrapper t35={2} workers={6} />
+        <OfferCardWrapper t35={2} workers={7} />
+        <OfferCardWrapper t35={2} workers={8} />
+      </AppGridContainer>
+      <AppGridContainer>
+        <OfferCardWrapper t35={3} workers={5} />
+        <OfferCardWrapper t35={3} workers={6} />
+        <OfferCardWrapper t35={3} workers={7} />
+        <OfferCardWrapper t35={3} workers={8} />
+      </AppGridContainer>
+    </>
   );
 }
 
@@ -39,7 +49,7 @@ function OfferCardWrapper(props: Props) {
 function TabIcon({ t35, workers }: Props) {
   return (
     <Box display="flex" justifyContent="center" width="100%">
-      <Box display="flex" gap={0.5} alignItems="center">
+      <Box display="flex" gap={1} alignItems="center">
         <Typography variant="h4">{workers}</Typography>
         <BoyOutlinedIcon fontSize="large" />
         <Divider orientation="vertical" flexItem />
