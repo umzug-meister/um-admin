9ca3d5fc9640ccd2e13055c98d16bbc13f10f2eb
Nikolai Kabiolskij <12097812+sputnyq@users.noreply.github.com>
2024-09-15 16:09:23 +0200

    zwischenstand

--- src/components/edit-order-components/OrderSources.tsx
+++ src/components/edit-order-components/OrderSources.tsx
@@ -1,22 +1,18 @@
 import { Alert, Grid2 } from '@mui/material';
 
-import React from 'react';
-
+import { orderSrcTypes } from 'um-types/constants';
+import { useCurrentOrder } from '../../hooks/useCurrentOrder';
 import OrderField from '../OrderField';
 import { AppCard } from '../shared/AppCard';
-import { OrderSrcType } from 'um-types';
-import { useCurrentOrder } from '../../hooks/useCurrentOrder';
 
 export default function OrderSources() {
   const order = useCurrentOrder();
   if (!order) return null;
 
-  const options = stringUnionToArray<OrderSrcType>('individuelle', 'Check24', 'MyHammer', 'MöbelTransport24', '');
-
   return (
     <Grid2 size={2}>
       <AppCard title="Auftragsquelle">
-        <OrderField path="src" label="Auftrag" select selectOptions={options} />
+        <OrderField path="src" label="Auftrag" select selectOptions={orderSrcTypes} />
         {order.src === 'individuelle' && (
           <Alert severity="warning">
             Für <strong>Check24, MyHammer, MöbelTransport24</strong> Auftrag setzen.
@@ -25,14 +21,4 @@ export default function OrderSources() {
       </AppCard>
     </Grid2>
   );
-}
-
-type ValueOf<T> = T[keyof T];
-
-type NonEmptyArray<T> = [T, ...T[]];
-
-type MustInclude<T, U extends T[]> = [T] extends [ValueOf<U>] ? U : never;
-
-function stringUnionToArray<T>() {
-  return <U extends NonEmptyArray<T>>(...elements: MustInclude<T, U>) => elements;
 }
