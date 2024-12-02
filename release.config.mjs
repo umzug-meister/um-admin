4c8f0ed2dfd5af87d9308f1323ff6e04bdb9ea4d
Nikolai K. <12097812+sputnyq@users.noreply.github.com>
2024-12-02 09:04:50 +0100

    feat: release config

---
+++ release.config.mjs
@@ -0,0 +1,30 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+        changelogTitle: '# Changelog',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md'],
+      },
+    ],
+  ],
+};
