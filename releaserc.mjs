8db6c4305c133faaa9140f39a82d8544fd1427c0
Nikolai K. <12097812+sputnyq@users.noreply.github.com>
2024-12-14 13:43:23 +0100

    feat: release config

--- releaserc.mjs
+++ releaserc.mjs
@@ -7,6 +7,12 @@ export default {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
     '@semantic-release/github',
   ],
 };
