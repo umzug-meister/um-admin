3f8416f6fdb50de1661a3eecd19985684a8101fc
Nikolai K. <12097812+sputnyq@users.noreply.github.com>
2025-02-09 11:21:35 +0100

    fix: update theme

--- src/theme.ts
+++ src/theme.ts
@@ -6,14 +6,6 @@ const theme = createTheme(
     palette: {
       background: {
         default: '#F3F6F9',
-        paper: '#fff',
-      },
-      primary: {
-        main: '#007FFF',
-      },
-
-      secondary: {
-        main: '#8c8c8c',
       },
     },
   },
