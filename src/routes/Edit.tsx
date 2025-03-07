ba0a0a18a6627cc8ed3f39a76c0db867e87beddd
Nikolai K. <12097812+sputnyq@users.noreply.github.com>
2025-03-07 18:19:04 +0100

    fix: override peer deps

--- src/routes/Edit.tsx
+++ src/routes/Edit.tsx
@@ -23,17 +23,16 @@ import { AppGridContainer } from '../components/shared/AppGridContainer';
 import { RootBox } from '../components/shared/RootBox';
 import { TabPanel } from '../components/shared/TabPanel';
 import { useLoadOrder } from '../hooks/useLoadOrder';
-
-// import { useSetDocumentTitle } from '../hooks/useSetDocumentTitle';
-// import { useUpdateCounter } from '../hooks/useUpdateCounter';
+import { useSetDocumentTitle } from '../hooks/useSetDocumentTitle';
+import { useUpdateCounter } from '../hooks/useUpdateCounter';
 
 export default function Edit() {
   const order = useLoadOrder();
 
   const [value, setValue] = useState(0);
 
-  // useUpdateCounter();
-  // useSetDocumentTitle(order?.id);
+  useUpdateCounter();
+  useSetDocumentTitle(order?.id);
 
   if (order == null) {
     return (
