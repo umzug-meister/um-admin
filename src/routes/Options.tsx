f74b862fb12c1050f2941dff3cc6e79805594bd1
Nikolai K. <12097812+sputnyq@users.noreply.github.com>
2025-05-05 20:53:19 +0200

    chore: remove unused option

--- src/routes/Options.tsx
+++ src/routes/Options.tsx
@@ -3,29 +3,23 @@ import { Grid2 } from '@mui/material';
 import { OptionInput } from '../components/OptionInput';
 import { AppCard } from '../components/shared/AppCard';
 import { AppGridContainer } from '../components/shared/AppGridContainer';
-import SendEmailFeatures from '../features/experimentals/SendEmailFeatures';
 
 export default function Options() {
   return (
     <AppGridContainer>
-      <Grid2 size={{ xs: 6, xl: 4 }}>
+      <Grid2 size={{ xs: 6 }}>
         <AppCard title="Berechnungsoptionen">
           <OptionInput name="boxCbm" label="Umzugskarton Volumen" type="number" endAdornment="m³" />
           <OptionInput name="kleiderboxCbm" label="Kleiderbox Volumen" type="number" endAdornment="m³" />
         </AppCard>
       </Grid2>
 
-      <Grid2 size={{ xs: 6, xl: 4 }}>
+      <Grid2 size={{ xs: 6 }}>
         <AppCard title="Server Einstellungen">
           <OptionInput name="origin" label="Standort" />
           <OptionInput name="dataPrivacyUrl" label="Datenschutz URL" />
           <OptionInput name="successUrl" label="Success URL" />
           <OptionInput name="boxCalculatorUrl" label="Kartonrechner URL" />
-        </AppCard>
-      </Grid2>
-      <Grid2 size={{ xs: 6, xl: 4 }}>
-        <AppCard title="Experimentelle Einstellungen">
-          <SendEmailFeatures />
         </AppCard>
       </Grid2>
     </AppGridContainer>
