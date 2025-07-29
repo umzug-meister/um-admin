607f89c26462afe35b6c56a0a357c69132860f73
Nikolai K. <12097812+sputnyq@users.noreply.github.com>
2025-07-29 20:45:04 +0200

    Fix: Update EmailActions.tsx

--- src/features/email/components/actions/EmailActions.tsx
+++ src/features/email/components/actions/EmailActions.tsx
@@ -127,7 +127,7 @@ export function EmailActions({ disabled: disabledButton }: Readonly<OrderActionB
             </MenuItemWithIcon>
           ) : null}
           <Divider />
-          <MenuItemWithIcon disabled={disabled} onClick={openRejectionDialog} text="Absage versenden">
+          <MenuItemWithIcon disabled={true} onClick={openRejectionDialog} text="Absage versenden">
             <CancelScheduleSendOutlinedIcon />
           </MenuItemWithIcon>
         </MenuList>
