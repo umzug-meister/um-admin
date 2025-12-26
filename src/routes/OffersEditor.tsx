78367bec118f0bf116348c714c1b6f74fb58af77
Nikolai K. <12097812+sputnyq@users.noreply.github.com>
2025-12-26 18:40:25 +0100

    Update OffersEditor.tsx
    Add four transporters options

--- src/routes/OffersEditor.tsx
+++ src/routes/OffersEditor.tsx
@@ -12,7 +12,8 @@ export default function OffersEditor() {
       <OfferContainer lkw={1} workers={[2, 3, 4]} />
       <OfferContainer lkw={2} workers={[3, 4, 5, 6, 7, 8]} />
       <OfferContainer lkw={3} workers={[5, 6, 7, 8]} />
-    </>
+      <OfferContainer lkw={4} workers={[7, 8, 9, 10]} />
+</>
   );
 }
 
